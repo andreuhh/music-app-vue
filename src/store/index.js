894e9485e81bbe870e53327e780de4a382d284d3
Andrea Guffi <andreaguffi92@gmail.com>
2021-08-04 22:09:19 +0200

    ok

--- src/store/index.js
+++ src/store/index.js
@@ -3,11 +3,15 @@ import { createStore } from 'vuex';
 export default createStore({
   state: {
     authModalShow: false,
+    userLoggedIn: false,
   },
   mutations: {
     toggleAuthModal: (state) => {
       state.authModalShow = !state.authModalShow;
     },
+    toggleAuth(state) {
+      state.userLoggedIn = !state.userLoggedIn;
+    }
   },
   getters: {
     // authModalShow: (state) => state.authModalShow,
