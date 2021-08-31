1fd2f74dc45126280c1a29ebc9746cf6d9cc50a1
Andrea Guffi <andreaguffi92@gmail.com>
2021-08-31 08:00:48 +0200

    ok

--- src/views/Manage.vue
+++ src/views/Manage.vue
@@ -16,227 +16,8 @@
             ></i>
           </div>
           <div class="p-6">
-            <div class="border border-gray-200 p-3 mb-4 rounded">
-              <div>
-                <h4 class="inline-block text-2xl font-bold">Song Name</h4>
-                <button
-                  class="
-                    ml-1
-                    py-1
-                    px-2
-                    text-sm
-                    rounded
-                    text-white
-                    bg-red-600
-                    float-right
-                  "
-                >
-                  <i class="fa fa-times"></i>
-                </button>
-                <button
-                  class="
-                    ml-1
-                    py-1
-                    px-2
-                    text-sm
-                    rounded
-                    text-white
-                    bg-blue-600
-                    float-right
-                  "
-                >
-                  <i class="fa fa-pencil-alt"></i>
-                </button>
-              </div>
-              <div>
-                <form>
-                  <div class="mb-3">
-                    <label class="inline-block mb-2">Song Title</label>
-                    <input
-                      type="text"
-                      class="
-                        block
-                        w-full
-                        py-1.5
-                        px-3
-                        text-gray-800
-                        border border-gray-300
-                        transition
-                        duration-500
-                        focus:outline-none
-                        focus:border-black
-                        rounded
-                      "
-                      placeholder="Enter Song Title"
-                    />
-                  </div>
-                  <div class="mb-3">
-                    <label class="inline-block mb-2">Genre</label>
-                    <input
-                      type="text"
-                      class="
-                        block
-                        w-full
-                        py-1.5
-                        px-3
-                        text-gray-800
-                        border border-gray-300
-                        transition
-                        duration-500
-                        focus:outline-none
-                        focus:border-black
-                        rounded
-                      "
-                      placeholder="Enter Genre"
-                    />
-                  </div>
-                  <button
-                    type="submit"
-                    class="py-1.5 px-3 rounded text-white bg-green-600"
-                  >
-                    Submit
-                  </button>
-                  <button
-                    type="submit"
-                    class="py-1.5 px-3 rounded text-white bg-gray-600"
-                  >
-                    Go Back
-                  </button>
-                </form>
-              </div>
-            </div>
-            <div class="border border-gray-200 p-3 mb-4 rounded">
-              <div>
-                <h4 class="inline-block text-2xl font-bold">Song Name</h4>
-                <button
-                  class="
-                    ml-1
-                    py-1
-                    px-2
-                    text-sm
-                    rounded
-                    text-white
-                    bg-red-600
-                    float-right
-                  "
-                >
-                  <i class="fa fa-times"></i>
-                </button>
-                <button
-                  class="
-                    ml-1
-                    py-1
-                    px-2
-                    text-sm
-                    rounded
-                    text-white
-                    bg-blue-600
-                    float-right
-                  "
-                >
-                  <i class="fa fa-pencil-alt"></i>
-                </button>
-              </div>
-            </div>
-            <div class="border border-gray-200 p-3 mb-4 rounded">
-              <div>
-                <h4 class="inline-block text-2xl font-bold">Song Name</h4>
-                <button
-                  class="
-                    ml-1
-                    py-1
-                    px-2
-                    text-sm
-                    rounded
-                    text-white
-                    bg-red-600
-                    float-right
-                  "
-                >
-                  <i class="fa fa-times"></i>
-                </button>
-                <button
-                  class="
-                    ml-1
-                    py-1
-                    px-2
-                    text-sm
-                    rounded
-                    text-white
-                    bg-blue-600
-                    float-right
-                  "
-                >
-                  <i class="fa fa-pencil-alt"></i>
-                </button>
-              </div>
-            </div>
-            <div class="border border-gray-200 p-3 mb-4 rounded">
-              <div>
-                <h4 class="inline-block text-2xl font-bold">Song Name</h4>
-                <button
-                  class="
-                    ml-1
-                    py-1
-                    px-2
-                    text-sm
-                    rounded
-                    text-white
-                    bg-red-600
-                    float-right
-                  "
-                >
-                  <i class="fa fa-times"></i>
-                </button>
-                <button
-                  class="
-                    ml-1
-                    py-1
-                    px-2
-                    text-sm
-                    rounded
-                    text-white
-                    bg-blue-600
-                    float-right
-                  "
-                >
-                  <i class="fa fa-pencil-alt"></i>
-                </button>
-              </div>
-            </div>
-            <div class="border border-gray-200 p-3 mb-4 rounded">
-              <div>
-                <h4 class="inline-block text-2xl font-bold">Song Name</h4>
-                <button
-                  class="
-                    ml-1
-                    py-1
-                    px-2
-                    text-sm
-                    rounded
-                    text-white
-                    bg-red-600
-                    float-right
-                  "
-                >
-                  <i class="fa fa-times"></i>
-                </button>
-                <button
-                  class="
-                    ml-1
-                    py-1
-                    px-2
-                    text-sm
-                    rounded
-                    text-white
-                    bg-blue-600
-                    float-right
-                  "
-                >
-                  <i class="fa fa-pencil-alt"></i>
-                </button>
-              </div>
-            </div>
+            <!-- COMPOSITION ITEM -->
+            <composition-item v-for="song in songs" :key="song.docID" />
           </div>
         </div>
       </div>
@@ -247,11 +28,32 @@
 <script>
 //import store from "@/store";
 import AppUpload from "@/components/Upload.vue";
+import CompositionItem from "@/components/CompositionItem.vue";
+import { songsCollection, auth } from "@/includes/firebase";
 
 export default {
   name: "manage",
   components: {
     AppUpload,
+    CompositionItem,
+  },
+  data() {
+    return {
+      songs: [],
+    };
+  },
+  async created() {
+    const snapshot = await songsCollection
+      .where("uid", "==", auth.currentUser.uid)
+      .get();
+
+    snapshot.forEach((document) => {
+      const song = {
+        ...document.data(),
+        docID: document.id,
+      };
+      this.songs.push(song);
+    });
   },
   beforeRouteLeave(to, from, next) {
     this.$refs.upload.cancelUploads();
