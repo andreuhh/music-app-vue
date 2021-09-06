dbf8b1f635270a58a87a48aa8a5ec1ca20ace458
Andrea Guffi <andreaguffi92@gmail.com>
2021-09-06 22:23:01 +0200

    animation page transition

--- src/components/SongItem.vue
+++ src/components/SongItem.vue
@@ -1,33 +1,42 @@
 <template>
-  <li
-    class="
-      flex
-      justify-between
-      items-center
-      p-3
-      pl-6
-      cursor-pointer
-      transition
-      duration-300
-      hover:bg-gray-50
-    "
-  >
-    <div>
-      <router-link
-        :to="{ name: 'song', params: { id: song.docID } }"
-        class="font-bold block text-gray-600"
-        >{{ song.modified_name }}</router-link
-      >
-      <span class="text-gray-500 text-sm">{{ song.name }}</span>
-    </div>
+  <router-link :to="{ name: 'song', params: { id: song.docID } }">
+    <li
+      class="
+        flex
+        justify-between
+        items-center
+        p-3
+        pl-6
+        cursor-pointer
+        transition
+        duration-300
+        hover:bg-gray-50
+      "
+    >
+      <!-- <div>
+        <router-link
+          :to="{ name: 'song', params: { id: song.docID } }"
+          class="font-bold block text-gray-600"
+          >{{ song.modified_name }}</router-link
+        >
+        <span class="text-gray-500 text-sm">{{ song.name }}</span>
+      </div> -->
 
-    <div class="text-gray-600 text-lg">
-      <span class="comments">
-        <i class="fa fa-comments text-gray-600"></i>
-        {{ song.comment_count }}
-      </span>
-    </div>
-  </li>
+      <div class="font-bold block text-gray-600">
+        {{ song.modified_name }}
+        <span class="text-gray-500 text-sm">{{ song.name }}</span>
+      </div>
+
+      <div class="text-gray-600 text-lg">
+        <!-- <router-link custom :to="{ name: "song", params: {id: song.docID}, hash: "#comments" }" v-slot="{ navigate }"> -->
+        <span class="comments">
+          <i class="fa fa-comments text-gray-600"></i>
+          {{ song.comment_count }}
+        </span>
+        <!-- </router-link> -->
+      </div>
+    </li>
+  </router-link>
 </template>
 
 <script>
